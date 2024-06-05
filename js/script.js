bedc6ff477de516958e57e4c4b16dad2f409980e
PISETH Sattya <pisethsattya33@gmail.com>
2024-06-05 17:25:46 +0700

    finished homework

--- js/script.js
+++ js/script.js
@@ -3,8 +3,19 @@ import {products} from "../data/products.js";
 
 let render = document.querySelector("#card-area");
 
-products.map(
-    (product) => {
+// products
+//     .filter((product) => product?.category?.name === "Shoes")
+
+//     .map(
+//     (product) => {
+//         render.innerHTML += cardComponent(product);
+//     }
+// )
+
+const BASE_URL = "http://127.0.0.1:5500/data/products.json";
+fetch(BASE_URL).then((res) => res.json())
+.then((data) => {
+    data.map((product) => {
         render.innerHTML += cardComponent(product);
-    }
-)
+    })
+})
